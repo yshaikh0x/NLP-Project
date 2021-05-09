1b2b41e20cdae4eea5c2ce3ec4c3a9acb93f91d8
unknown <yshaikh0x@gmail.com>
2021-05-09 14:35:15 -0400

    NLP project

---
+++ src/server/index.js
@@ -0,0 +1,31 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+
+const app = express()
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+    // res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
+   })
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
+
+
+console.log(checkForName);
+
+alert("hello!!")
+
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path")
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+        }
+        ],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+    })
+    ]
+    }}
